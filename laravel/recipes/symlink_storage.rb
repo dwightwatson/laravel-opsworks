77948580054e8b9bf3d259bb2b88864350040cc9
Dwight Watson <dwightwatson@me.com>
2015-08-14 14:43:56 +1000

    Wrap symlink storage directory recipe in a script

--- laravel/recipes/symlink_storage.rb
+++ laravel/recipes/symlink_storage.rb
@@ -1,10 +1,12 @@
 node[:deploy].each do |application, deploy|
-  interpreter "bash"
-  user "root"
-  cwd "#{deploy[:deploy_to]}"
-  code <<-EOH
-    mv current/storage/* shared
-    rm -rf current/storage
-    ln -s shared current/storage
-  EOH
+  script "symlink_storage"
+    interpreter "bash"
+    user "root"
+    cwd "#{deploy[:deploy_to]}"
+    code <<-EOH
+      mv current/storage/* shared
+      rm -rf current/storage
+      ln -s shared current/storage
+    EOH
+  end
 end
