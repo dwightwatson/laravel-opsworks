9d51ed347436c1489888333f820a75e39f58a292
Chad Ertel <chadertel@users.noreply.github.com>
2016-11-13 12:04:13 -0600

    added group read/write permissions
    even with a global read/write it still appears that in some cases the group was not properly able to write to directories under random deployments.

--- laravel/recipes/storage_permissions.rb
+++ laravel/recipes/storage_permissions.rb
@@ -5,6 +5,8 @@ node[:deploy].each do |application, deploy|
     cwd "#{deploy[:deploy_to]}/current"
     code <<-EOH
     chmod -R 777 storage/app storage/framework storage/logs
+    chmod gu+w bootstrap/cache
+    chmod gu+w storage/framework/sessions
     EOH
   end
 end
